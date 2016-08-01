fbfc43be2549aa503e8037c6431d708070bb5878
rsta80 <arst80@gmail.com>
2016-08-01 17:44:40 -0500

    implemented solution of Conway's game of life using angular and TDD :)

--- client/app/scripts/game_of_life/controller.js
+++ client/app/scripts/game_of_life/controller.js
@@ -1,16 +1,131 @@
 'use strict';
 
 angular.module('Game_of_life')
-.controller('game_of_life', function ($scope) {
+    .controller('game_of_life', function ($scope) {
 
-  $scope.controller_loaded = 'Game of life loaded!';
+        $scope.controller_loaded = 'Game of life loaded!';
+        $scope.matrix = [0];
+        $scope.glidder_pattern = [[1, [1]], [2, [2, 3]], [3, [1, 2]]];
+        $scope.r_pentomino = [[3, [4, 5]], [4, [3, 4]], [5, [4]]];
+        $scope.pulsar_pattern = [
+            [1, [3, 4, 10, 11]],
+            [2, [4, 5, 9, 10]],
+            [3, [1, 4, 6, 8, 10, 13]],
+            [4, [1, 2, 3, 5, 6, 8, 9, 11, 12, 13]],
+            [5, [2, 4, 6, 8, 10, 12]],
+            [6, [3, 4, 5, 9, 10, 11]],
+            [8, [3, 4, 5, 9, 10, 11]],
+            [9, [2, 4, 6, 8, 10, 12]],
+            [10, [1, 2, 3, 5, 6, 8, 9, 11, 12, 13]],
+            [11, [1, 4, 6, 8, 10, 13]],
+            [12, [4, 5, 9, 10]],
+            [13, [3, 4, 10, 11]]];
 
 
-})
-.config(function ($routeProvider) {
-  $routeProvider
-  .when('/game_of_life', {
-    templateUrl: 'scripts/game_of_life/views/game_of_life.html',
-    controller: 'game_of_life'
-  });
-});
+        $scope.initialize_matrix = function (row_number, col_number) {
+
+            $scope.matrix = [];
+            for (var row = 0; row < row_number; row++) {
+                $scope.matrix[row] = [];
+                for (var col = 0; col < col_number; col++) {
+                    $scope.matrix[row][col] = 0;
+                }
+            }
+            return $scope.matrix;
+        };
+
+        $scope.matrix_turned_on = function (array_turn_on) {
+
+            for (var row = 0; row < array_turn_on.length; row++) {
+                updated_row($scope.matrix[array_turn_on[row][0]], array_turn_on[row][1]);
+            }
+
+            return $scope.matrix;
+        };
+
+        $scope.iterate_matrix = function (matrix) {
+
+            var old_matrix = matrix;
+            $scope.matrix = [];
+
+            for (var row = 0; row < old_matrix.length; row++) {
+                $scope.matrix[row] = [];
+                for (var col = 0; col < old_matrix[row].length; col++) {
+                    $scope.matrix[row][col] = cell_change_state(row, col, old_matrix);
+                }
+            }
+
+            return $scope.matrix;
+        };
+
+        $scope.turn_single_cell = function (cell) {
+
+            var cell_array = cell.split(',');
+
+            if ($scope.matrix[cell_array[0]][cell_array[1]] === 0) {
+                $scope.matrix[cell_array[0]][cell_array[1]] = 1;
+            }
+            else if ($scope.matrix[cell_array[0]][cell_array[1]] === 1) {
+                $scope.matrix[cell_array[0]][cell_array[1]] = 0;
+            }
+            return $scope.matrix;
+
+        };
+
+        $scope.set_demo_pattern = function (pattern, matrix_long) {
+            $scope.initialize_matrix(matrix_long, matrix_long);
+            $scope.matrix_turned_on(pattern);
+        };
+
+        function updated_row(row, cells_in_row) {
+
+            for (var cell = 0; cell < cells_in_row.length; cell++) {
+                row[cells_in_row[cell]] = 1;
+            }
+
+        }
+
+        function cell_change_state(row, col, matrix) {
+
+            var count = count_cell_neighbors(row, col, matrix);
+            if (matrix[row][col] === 1 && !( (count === 2) || (count === 3) )) {
+                return 0;
+            }
+            else if (matrix[row][col] === 0 && count === 3) {
+                return 1;
+            }
+            return matrix[row][col];
+
+        }
+
+        function count_cell_neighbors(row, col, matrix) {
+
+            var count = 0;
+            for (var row_neighbor = (row - 1); row_neighbor <= (row + 1); row_neighbor++) {
+                if (row_neighbor === -1 || row_neighbor === matrix.length) {
+                    continue;
+                }
+                for (var col_neighbor = (col - 1); col_neighbor <= (col + 1); col_neighbor++) {
+                    if (col_neighbor === -1 || col_neighbor === matrix[row].length) {
+                        continue;
+                    }
+                    if (row_neighbor === row && col_neighbor === col) {
+                        continue;
+                    }
+                    if (matrix[row_neighbor][col_neighbor] === 1) {
+                        count++;
+                    }
+                }
+            }
+            return count;
+
+        }
+
+    })
+    .config(function ($routeProvider) {
+        $routeProvider
+            .when('/game_of_life', {
+                templateUrl: 'scripts/game_of_life/views/game_of_life.html',
+                controller: 'game_of_life'
+            });
+    });
--- client/app/scripts/game_of_life/specs/controller_spec.js
+++ client/app/scripts/game_of_life/specs/controller_spec.js
@@ -2,50 +2,239 @@
 
 describe('Controller: game_of_life', function () {
 
-  beforeEach(module('Game_of_life'));
+    beforeEach(module('Game_of_life'));
 
-  var controller;
-  var scope;
+    var controller;
+    var scope;
 
-  beforeEach(inject(function ($rootScope, $controller) {
-    scope = $rootScope.$new();
-    controller = $controller('game_of_life', { $scope: scope });
-  }));
-
-  describe('On instance', function(){
-    it('should set "controller_loaded" variable in scope', function() {
-      expect(scope.controller_loaded).toContain('loaded');
-    });
-  });
-
-  describe('when going to /game_of_life', function() {
-
-    var route, location, rootScope, httpBackend;
-
-    beforeEach(inject(function($route, $location, $rootScope, $httpBackend){
-      route = $route;
-      location = $location;
-      rootScope = $rootScope;
-      httpBackend = $httpBackend;
-
-      httpBackend.when('GET', 'scripts/game_of_life/views/game_of_life.html').respond('<div></div>');
+    beforeEach(inject(function ($rootScope, $controller) {
+        scope = $rootScope.$new();
+        controller = $controller('game_of_life', {$scope: scope});
     }));
 
-    afterEach(function() {
-      httpBackend.verifyNoOutstandingExpectation();
-      httpBackend.verifyNoOutstandingRequest();
+    describe('On instance', function () {
+
+        var matrix = [];
+
+        it('should set "controller_loaded" variable in scope', function () {
+            expect(scope.controller_loaded).toContain('loaded');
+        });
+
+        it('should initialize  matrix with 4 rows and 8 columns containing 0 values', function () {
+            expect(scope.initialize_matrix(4, 8)).toEqual(
+                [
+                    [0, 0, 0, 0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0, 0, 0, 0]
+                ]
+            );
+        });
+
+        it('should initialize  matrix with 3 rows and 6 columns containing 0 values', function () {
+            expect(scope.initialize_matrix(3, 6)).toEqual(
+                [
+                    [0, 0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0, 0]
+                ]
+            );
+        });
+
+        it('should turn on input cells noted by [row, [columnss]]', function () {
+
+            scope.initialize_matrix(4,8);
+            var initial_cells_to_turn_on = [[0, [3,4]], [1, [2, 5]], [2, [1,2,3,4,5,6]],[3,[2,5]]];
+
+            expect(scope.matrix_turned_on(initial_cells_to_turn_on)).toEqual(
+                [
+                    [0, 0, 0, 1, 1, 0, 0, 0],
+                    [0, 0, 1, 0, 0, 1, 0, 0],
+                    [0, 1, 1, 1, 1, 1, 1, 0],
+                    [0, 0, 1, 0, 0, 1, 0, 0]
+                ]
+            );
+        });
+
+        it('should turn on input cells noted by [row, [columnss]]', function () {
+
+            scope.initialize_matrix(4,8);
+            var initial_cells_to_turn_on = [
+                [1, [3]],
+                [2, [2, 3]]
+            ];
+
+            matrix = scope.matrix_turned_on(initial_cells_to_turn_on);
+
+            expect(matrix).toEqual(
+                [
+                    [0, 0, 0, 0, 0, 0, 0, 0],
+                    [0, 0, 0, 1, 0, 0, 0, 0],
+                    [0, 0, 1, 1, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0, 0, 0, 0]
+                ]
+            );
+
+        });
+
+        it('should iterate to next generation', function () {
+
+            expect(scope.iterate_matrix(matrix)).toEqual(
+                [
+                    [0, 0, 0, 0, 0, 0, 0, 0],
+                    [0, 0, 1, 1, 0, 0, 0, 0],
+                    [0, 0, 1, 1, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0, 0, 0, 0]
+                ]
+            );
+        });
+
+        it('should turn on input cells noted by [row, [columnss]]', function () {
+
+            scope.initialize_matrix(5,5);
+            var initial_cells_to_turn_on = [
+                [2, [1,2,3]]
+            ];
+
+            matrix = scope.matrix_turned_on(initial_cells_to_turn_on);
+
+            expect(matrix).toEqual(
+                [
+                    [0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0],
+                    [0, 1, 1, 1, 0],
+                    [0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0]
+                ]
+            );
+
+        });
+
+        it('should iterate to next generation', function () {
+
+            expect(scope.iterate_matrix(matrix)).toEqual(
+                [
+                    [0, 0, 0, 0, 0],
+                    [0, 0, 1, 0, 0],
+                    [0, 0, 1, 0, 0],
+                    [0, 0, 1, 0, 0],
+                    [0, 0, 0, 0, 0]
+                ]
+            );
+        });
+
+        it('should turn on input cells for Beacon (period 2)', function () {
+
+            scope.initialize_matrix(6,6);
+            var initial_cells_to_turn_on = [
+                [1, [1,2]],
+                [2,[1]],
+                [3, [4]],
+                [4, [3,4]]
+            ];
+
+            matrix = scope.matrix_turned_on(initial_cells_to_turn_on);
+
+            expect(matrix).toEqual(
+                [
+                    [0, 0, 0, 0, 0, 0],
+                    [0, 1, 1, 0, 0, 0],
+                    [0, 1, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 1, 0],
+                    [0, 0, 0, 1, 1, 0],
+                    [0, 0, 0, 0, 0, 0]
+                ]
+            );
+
+        });
+
+        it('should iterate to next generation of Beacon (period 2)', function () {
+
+            matrix = scope.iterate_matrix(matrix);
+
+            expect(matrix).toEqual(
+                [
+                    [0, 0, 0, 0, 0, 0],
+                    [0, 1, 1, 0, 0, 0],
+                    [0, 1, 1, 0, 0, 0],
+                    [0, 0, 0, 1, 1, 0],
+                    [0, 0, 0, 1, 1, 0],
+                    [0, 0, 0, 0, 0, 0]
+                ]
+            );
+        });
+
+        it('should iterate to next generation of Beacon (period 2)', function () {
+
+            matrix = scope.iterate_matrix(matrix);
+
+            expect(matrix).toEqual(
+                [
+                    [0, 0, 0, 0, 0, 0],
+                    [0, 1, 1, 0, 0, 0],
+                    [0, 1, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 1, 0],
+                    [0, 0, 0, 1, 1, 0],
+                    [0, 0, 0, 0, 0, 0]
+                ]
+            );
+        });
+
+        it('should turn on/off a single cell of the matrix', function () {
+
+            scope.initialize_matrix(3,5);
+
+            expect(scope.turn_single_cell('1,3')).toEqual(
+                [
+                    [0, 0, 0, 0, 0],
+                    [0, 0, 0, 1, 0],
+                    [0, 0, 0, 0, 0]
+                ]
+            );
+        });
+
+        it('should turn on/off a single cell of the matrix', function () {
+
+            scope.initialize_matrix(2,6);
+
+            expect(scope.turn_single_cell('1,5')).toEqual(
+                [
+                    [0, 0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0, 1]
+                ]
+            );
+        });
+
     });
 
-    it('should use game_of_life.html and controller', function() {
-      expect(route.current).toBeUndefined();
+    describe('when going to /game_of_life', function () {
 
-      location.path('/game_of_life');
+        var route, location, rootScope, httpBackend;
 
-      httpBackend.flush();
+        beforeEach(inject(function ($route, $location, $rootScope, $httpBackend) {
+            route = $route;
+            location = $location;
+            rootScope = $rootScope;
+            httpBackend = $httpBackend;
 
-      expect(route.current.templateUrl).toBe('scripts/game_of_life/views/game_of_life.html');
-      expect(route.current.controller).toBe('game_of_life');
+            httpBackend.when('GET', 'scripts/game_of_life/views/game_of_life.html').respond('<div></div>');
+        }));
+
+        afterEach(function () {
+            httpBackend.verifyNoOutstandingExpectation();
+            httpBackend.verifyNoOutstandingRequest();
+        });
+
+        it('should use game_of_life.html and controller', function () {
+            expect(route.current).toBeUndefined();
+
+            location.path('/game_of_life');
+
+            httpBackend.flush();
+
+            expect(route.current.templateUrl).toBe('scripts/game_of_life/views/game_of_life.html');
+            expect(route.current.controller).toBe('game_of_life');
+        });
     });
-  });
 
 });
